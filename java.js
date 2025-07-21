d5e70dba8b8003fdd3d23de061170b371c041304
AbdelhalimA0 <abdelhalim.abdelwahab2@gmail.com>
2025-07-21 19:09:26 +0300

    session count

--- java.js
+++ java.js
@@ -1,5 +1,16 @@
-let Voltage=window.prompt('Voltage');
-let Resistance=window.prompt('Resistance');
-console.log('V = '+Voltage);
-console.log('R = '+Resistance);
-console.log('I = ' +Voltage/Resistance);
+// let Voltage=window.prompt('Voltage');
+// let Resistance=window.prompt('Resistance');
+// console.log('V = '+Voltage);
+// console.log('R = '+Resistance);
+// console.log('I = ' +Voltage/Resistance);
+
+let name1=window.prompt('name1');
+let NAME1=[name1];
+let name2=window.prompt('name2');
+let NAME2=[name2];
+let name3=window.prompt('name3');
+let NAME3=[name3];
+console.log(NAME1);
+console.log(NAME2);
+console.log(NAME3);
+console.log(NAME1.concat(NAME2,NAME3).length);
