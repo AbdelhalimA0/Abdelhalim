aedaf06867ef8a0db0e34f693d1c51c65984400b
AbdelhalimA0 <abdelhalim.abdelwahab2@gmail.com>
2025-07-29 18:58:06 +0300

    functions

--- java.js
+++ java.js
@@ -16,18 +16,32 @@
 // console.log(NAME1.concat(NAME2,NAME3));
 // console.log(NAME1.concat(NAME2,NAME3).length);
 
-function calc() {
-  let a = parseFloat(document.querySelector("#num1").value);
-  let b = parseFloat(document.querySelector("#num2").value);
-  let op = document.querySelector("#operator").value;
-  let r;
+// function calc() {
+//   let a = parseFloat(document.querySelector("#num1").value);
+//   let b = parseFloat(document.querySelector("#num2").value);
+//   let op = document.querySelector("#operator").value;
+//   let r;
 
-  if (op === "+") r = a + b;
-  else if (op === "-") r = a - b;
-  else if (op === "*") r = a * b;
-  else if (op === "/") r = b !== 0 ? a / b : "Error: ÷0";
-  else r = "wrong";
+//   if (op === "+") r = a + b;
+//   else if (op === "-") r = a - b;
+//   else if (op === "*") r = a * b;
+//   else if (op === "/") r = b !== 0 ? a / b : "Error: ÷0";
+//   else r = "wrong";
 
-  document.querySelector("#result").textContent = r;
+//   document.querySelector("#result").textContent = r;
+// }
+
+let x = 5;
+let y = 100;
+
+for (let i = 0; i < y; i++) {
+  if (x * i < y) {
+    console.log(x * i);
+  } else {
+    break;
+  }
 }
 
+let checkNameLength = name => name.length % 2 === 0 ? "even" : "odd";
+console.log(checkNameLength("Ahmed")); 
+console.log(checkNameLength("Abdelhalim"));  
