bae72b2a246c5191bcb3e823fba627afbc1716ab
AbdelhalimA0 <abdelhalim.abdelwahab2@gmail.com>
2025-07-31 19:01:11 +0300

    objecct

--- java.js
+++ java.js
@@ -31,18 +31,62 @@
 //   document.querySelector("#result").textContent = r;
 // }
 
-let x = prompt("the number for the multiplication table (x):");
-let y = prompt("upper limit (y):");
+// let x = prompt("the number for the multiplication table (x):");
+// let y = prompt("upper limit (y):");
 
-for (let i = 0; i < y; i++) {
-  if (x * i < y) {
-    console.log(x * i);
-  } else {
-    console.log('wrong numbers');
+// for (let i = 0; i < y; i++) {
+//   if (x * i < y) {
+//     console.log(x * i);
+//   } else {
+//     console.log('wrong numbers');
+//   }
+// }
+
+// let name = prompt("Enter your name:");
+// let checkNameLength = name => name.length % 2 === 0 ? "even" : "odd";
+// console.log(`length of the name "${name}" is: ${checkNameLength(name)}`);
+  
+
+let person = {
+  name: "Moo",
+  age: 20,
+  city: "Alex",
+  information() {
+    console.log(`I am ${this.name}, ${this.age} years old, from ${this.city}.`);
   }
+};
+
+let student = Object.create(person);
+Object.assign(student, {
+ name : "Abdelhalim",
+age : 22,
+university : "helwan University",
+major : "science",
+information : function () {
+  console.log(`I am ${this.name}, a ${this.age}-year-old ${this.major} student at ${this.university}.`);
+},
+})
+
+let doctor = Object.create(person);
+Object.assign(doctor,{
+name : "Dr. Amr",
+age : 50,
+teach : "chem department",
+information : function () {
+  console.log(`I am ${this.name}, doctor at ${this.teach}.`);
+
+},
+});
+person.information();
+student.information();
+doctor.information();
+
+
+function findMinMax(arr = []) {
+  return [Math.min(...arr), Math.max(...arr)];
 }
 
-let name = prompt("Enter your name:");
-let checkNameLength = name => name.length % 2 === 0 ? "even" : "odd";
-console.log(`length of the name "${name}" is: ${checkNameLength(name)}`);
-  
+
+const [min, max] = findMinMax([4, 6, 2,22,3245,45,1, 3, 10]);
+console.log(`Min: ${min}, Max: ${max}`);
+
